740a1d5ccf5a53e3b6ffb8345057eea5343f5f87
Dayane <acdayane@gmail.com>
2022-11-16 20:39:44 -0300

    style: main+ cashIn + cashOut pages

--- src/pages/CashOutPage.js
+++ src/pages/CashOutPage.js
@@ -1,5 +1,90 @@
-export default function CashOutPage () {
+import { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function CashOutPage() {
+
+    const [value, setValue] = useState("");
+    const [description, setDescription] = useState("");
+    const navigate = useNavigate();
+
+    function newCashIn(e) {
+
+        e.preventDefault();
+    }
+
     return (
-        <h1>cash out page</h1>
+
+        <Container>
+            <h1>Nova saída</h1>
+            <form onSubmit={newCashIn}>
+                <input
+                    type="number"
+                    placeholder="Valor"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Descrição"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    required
+                />
+                <button type="submit">Salvar saída</button>
+            </form>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    background-color: #8C11BE;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+h1 {
+    width: 90vw;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 700;
+    color: #FFFFFF;
+    font-size: 26px;
+    margin: 20px 0;
+}   
+form {
+    display: flex;
+    flex-direction: column;
+}
+input {
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    width: 90vw;
+    height: 58px;
+    margin-bottom: 10px;
+    color: #363636;
+    font-family:'Lexend Deca', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+}
+input::placeholder {
+    font-family:'Lexend Deca', sans-serif;
+    font-weight: 400;
+    color: #363636;
+    font-size: 18px;
+}
+button {
+    border: 1px solid #A328D6;
+    border-radius: 5px;
+    width: 92vw;
+    height: 48px;
+    background: #A328D6;
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 700;
+    color: #FFFFFF;
+    font-size: 20px;
+    cursor: pointer;
+}
+`
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,104 @@
-export default function MainPage () {
+import styled from "styled-components";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function MainPage() {
+
+    const [name, setName] = useState("Dayane");
+
     return (
-        <h1>main page</h1>
+        <Container>
+            <Header>
+                <h1>Olá, {name}!</h1>
+                <ion-icon name="log-out-outline"></ion-icon>
+            </Header>
+            <BoxMain>
+                <h2>Não há registros de entrada ou saída</h2>
+            </BoxMain>
+            <ContainerBotton>
+                <BoxCashInOut>
+                    <Link to="/nova-entrada" style={{ textDecoration: "none" }}>
+                        <ion-icon name="add-circle-outline"></ion-icon>
+                        <p>Nova entrada</p>
+                    </Link>
+                </BoxCashInOut>
+                <BoxCashInOut>
+                    <Link to="/nova-saida" style={{ textDecoration: "none" }}>
+                        <ion-icon name="remove-circle-outline"></ion-icon>
+                        <p>Nova saída</p>
+                    </Link>
+                </BoxCashInOut>
+            </ContainerBotton>
+        </Container >
     )
 }
+
+const Container = styled.div`
+    background-color: #8C11BE;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Header = styled.div`
+    width: 90vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px;
+h1 {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 700;
+    color: #FFFFFF;
+    font-size: 26px;
+}
+ion-icon {
+    font-size: 30px;
+    color: #FFFFFF;
+}
+`
+const BoxMain = styled.div`
+    background-color: #FFFFFF;
+    height: auto;
+    min-height: 60vh;
+    width: 90vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 5px;
+    margin-bottom: 15px;
+h2 {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    color: #868686;
+    font-size: 20px;
+    text-align: center;
+    margin: auto;
+}
+`
+const ContainerBotton = styled.div`
+    width: 90vw;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+`    
+const BoxCashInOut = styled.div`       
+    border-radius: 5px;
+    height: 20vh;
+    width: 44vw;
+    background-color: #A328D6;
+    display: flex;
+    justify-content: space-between;
+p {
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 700;
+    color: #FFFFFF;
+    font-size: 17px;
+}
+ion-icon {
+    font-size: 30px;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+`
